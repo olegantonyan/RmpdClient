ab912f189ad9e96db54b748140c651b783513e66
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-08-14 20:18:07 +0200

    layout imporements

--- app/src/main/java/ru/slon_ds/rmpdclient/MainActivity.java
+++ app/src/main/java/ru/slon_ds/rmpdclient/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.slon_ds.rmpdclient;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.VideoView;
@@ -16,12 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE);
         setContentView(R.layout.activity_main);
 
         VideoView video_view = (VideoView) findViewById(R.id.videoView);
         video_view.setZOrderOnTop(true);
         video_view.requestFocus();
+
+        video_view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                ActionBar a = getSupportActionBar();
+                if (a != null) a.hide();
+                android.app.ActionBar b = getActionBar();
+                if (b != null) b.hide();
+                return true;
+            }
+        });
+
+        /*ActionBar a = getSupportActionBar();
+        if (a != null) a.hide();
+        android.app.ActionBar b = getActionBar();
+        if (b != null) b.hide();*/
+
+
         main = new Main(new PlayerWrapper(video_view));
     }
 
